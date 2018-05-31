cc8090661c07448aa065e02e606b2660f1f323db
38 <xxx@xxx.xxx>
2018-05-31 21:52:37 +0800

    add var_dump lab

---
+++ general/var_dump_lab.php
@@ -0,0 +1,13 @@
+
+<?php
+    $a=11;
+    $b=true;
+    $c= array(1,33,5, array(33,56,7));
+    var_dump($a, $b, $c);
+    echo '<br>';
+    print_r($a);
+    echo '<br>';
+    print_r($b); // true gets 1 when using print_r
+    echo '<br>';
+    print_r($c);
+?>
