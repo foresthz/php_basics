4f74eca97851d1e2c2785858ce5840800224b4b1
38 <xxx@xxx.xxx>
2018-06-01 14:41:29 +0800

    invoke react

---
+++ html/script/script_react.php
@@ -0,0 +1,8 @@
+
+<!-- <script src="https://cdn.bootcss.com/react/15.6.1/react.js"></script> -->
+<script src="https://cdn.bootcss.com/react/15.6.1/react-dom.js"></script>
+
+
+<?php
+    echo 3+33+10;
+?>
